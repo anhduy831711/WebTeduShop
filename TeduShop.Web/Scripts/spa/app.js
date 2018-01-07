76f55e93f9905119655c569ce31140331c50bcbd
Anh Duy Trần <duylovegiang1996@gmail.com>
2018-01-08 01:04:10 +0700

    unit 17 learn angular js how to add module and controller

---
+++ TeduShop.Web/Scripts/spa/app.js
@@ -0,0 +1,12 @@
+﻿/// <reference path="../plugins/angular/angular.js" />
+
+var myApp = angular.module('MyModule', []);
+
+myApp.controller("MyController", MyController);
+
+MyController.$inject = ['$scope'];
+
+function MyController($scope)
+{
+    $scope.message = "My Message";
+}
