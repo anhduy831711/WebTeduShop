9013093c0082c0f666af2b0c021219b471455562
Anh Duy Trần <duylovegiang1996@gmail.com>
2018-01-13 00:16:45 +0700

    routing in ui router

--- TeduShop.Web/app/app.js
+++ TeduShop.Web/app/app.js
@@ -1,1 +1,17 @@
-﻿
+﻿/// <reference path="/Assets/admin/libs/angular/angular.js" />
+
+(function () {
+    angular.module('tedushop', ['tedushop.products', 'tedushop.common']).config(config);
+
+    config.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+    function config($stateProvider, $urlRouterProvider) {
+        $stateProvider.state('home', {
+            url: "/admin",
+            templateUrl: "/app/components/home/HomeView.html",
+            controller: "HomeController"
+        })
+
+        $urlRouterProvider.otherwise('/admin');
+    }
+})();
