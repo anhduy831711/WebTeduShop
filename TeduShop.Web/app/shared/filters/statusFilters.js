b9f2655bd1ef39717025684d58e6382e86df2f5e
Anh Duy Trần <duylovegiang1996@gmail.com>
2018-01-19 15:37:02 +0700

    filter in angular js

---
+++ TeduShop.Web/app/shared/filters/statusFilters.js
@@ -0,0 +1,15 @@
+﻿(function (app) {
+    app.filter('statusFilter', function () {
+        return function(input)
+        {
+            if(input)
+            {
+                return 'Kích Hoạt';
+            }
+            else
+            {
+                return 'Khóa';
+            }
+        }
+    })
+})(angular.module('tedushop.common'));
