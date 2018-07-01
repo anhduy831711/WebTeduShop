f75c90b39e631c67e88d4e44391f40b4c771c5f5
EagleTran-PC\EagleTran <tranduy681996@gmail.com>
2018-07-01 17:23:24 +0700

    Validate Form Add ProductCategory

--- TeduShop.Web/app/components/product_categories/productCategoryAddController.js
+++ TeduShop.Web/app/components/product_categories/productCategoryAddController.js
@@ -6,11 +6,9 @@
         $scope.productCategory = {
             CreatedDate: new Date(),
             Status: true,
-            Name: 'Danh Mục 1',
-            Alias:'danh-muc-1'
         }
         $scope.AddProductCategory = AddProductCategory;
-
+        $scope.regularName = /^\w{3,10}$/;
         function AddProductCategory()
         {
             apiService.post('api/productcategory/create', $scope.productCategory, function (result) {
